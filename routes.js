3a3afe73e612a596b50ecf0de9f6da101037443c
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-02 10:49:29 -0300

    sistema de login feito

--- routes.js
+++ routes.js
@@ -8,5 +8,6 @@ route.get('/', homeController.index);
 
 route.get('/login/index', loginController.index)
 route.post('/login/register', loginController.register)
+route.post('/login/login', loginController.login)
 
 module.exports = route;
