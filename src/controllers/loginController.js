cf09717b8c3d07cd09cf1d6e6bf2fa2499b710be
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-02 21:08:44 -0300

    pagina de login feita

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,10 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-    res.render('login')
+    if (req.session.user) {
+        return res.render('login-logado')
+    }
+    return res.render('login')
 }
 
 exports.register = async function (req, res) {
@@ -47,3 +50,7 @@ exports.login = async function(req, res) {
         return res.render('404');
     }
 };
+exports.logout = function (req, res) {
+    req.session.destroy()
+    res.redirect('/')
+}
