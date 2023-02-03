e01a0fb3cc0bd8385d742ad1274c8136d3586b22
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-03 17:27:09 -0300

    termieni o cadastro

--- routes.js
+++ routes.js
@@ -12,6 +12,7 @@ route.get('/login/index', loginController.index)
 route.post('/login/register', loginController.register)
 route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
-route.get('/contato/index', loginRequired, contatoController.index)
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
 
 module.exports = route;
---
+++ src/models/ContatoModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const { isEmail } = require('validator');
+const ContatoSchema = new mongoose.Schema({
+    nome: {type: String, required: true},
+    sobrenome: {type: String, required: false, default: ''},
+    email: {type: String, required: false, default: ''},
+    telefone: {type: String, required: false, default: ''},
+    criadoEm: {type: Date, default: Date.now},
+})
+const ContatoModel = mongoose.model('Contato', ContatoSchema)
+
+class Contato {
+    constructor(body) {
+        this.body = body;
+        this.errors = [],
+        this.contato = null;
+    }
+    async register() {
+        this.valida()
+        if (this.errors.length > 0) {
+            return
+        }
+        this.contato = await ContatoModel.create(this.body)
+    }
+    valida() {
+        this.cleanUp()
+        if (this.body.email && !isEmail(this.body.email)) this.errors.push('Email inválido!')
+        if (!this.body.nome) {
+            this.errors.push('Nome é um campo obrigatório!')
+        }
+        if(!this.body.email && !this.body.telefone) {
+            this.errors.push('Pelo meno um contato precisa ser enviado')
+        }
+    }
+    async login() {
+        this.valida();
+        if(this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email });
+    
+        if(!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
+        }
+    }
+    cleanUp() {
+        for (const key in this.body) {
+            if(typeof this.body[key] !== 'string') {
+            this.body[key] = '';
+            }
+        }
+    }
+}
+
+module.exports = Contato
